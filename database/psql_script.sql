fd8e3f898436e6b2cf9edb2ed8a4d01d6969b42b
Nyan Wun Paing <nwp.nyan@gmail.com>
2020-03-24 17:28:06 +0800

    Update SQL creation

--- database/psql_script.sql
+++ database/psql_script.sql
@@ -2,27 +2,26 @@ DROP TABLE IF EXISTS Restaurant CASCADE;
 DROP TABLE IF EXISTS Food CASCADE;
 DROP TABLE IF EXISTS Categories CASCADE;
 DROP TABLE IF EXISTS Menu CASCADE;
-DROP TABLE IF EXISTS From CASCADE;
+DROP TABLE IF EXISTS FromMenu CASCADE;
 DROP TABLE IF EXISTS Order CASCADE;
 
 
 CREATE TABLE Restaurants (
 restaurantID    INTEGER,
 name            VARCHAR(100)         NOT NULL,
-location        VARCHAR(255)        NOT NUll,
-minThreshold    INTEGER DEFAULT '0' NOT NULL,
+location        VARCHAR(255)         NOT NUll,
+minThreshold    INTEGER DEFAULT '0'  NOT NULL,
 PRIMARY KEY (RestaurantID)
 );
 
 CREATE TABLE Food(
-foodName        VARCHAR(100)        NOT NULL,
-availability    BINARY              NOT NULL,
-price           INTEGER             NOT NULL,
-dailyLimit      INTEGER DEFAULT '50'NOT NULL,
+foodName        VARCHAR(100)         NOT NULL,
+availability    BOOLEAN              NOT NULL,
+price           INTEGER              NOT NULL,
+dailyLimit      INTEGER DEFAULT '50' NOT NULL,
 RestaurantID    INTEGER,
 PRIMARY KEY (RestaurantID, foodName),
-FOREIGN KEY (RestaurantID) REFERENCES Restaurants (RestaurantID)
-ON DELETE CASCADE
+FOREIGN KEY (RestaurantID) REFERENCES Restaurants (RestaurantID) ON DELETE CASCADE
 );
 
 CREATE TABLE Categories (
@@ -37,12 +36,12 @@ PRIMARY KEY (restaurantID,foodName)
 );
 
 CREATE TABLE Order (
-orderID             INTEGER         NOT NULL,
-deliveryFee         INTEGER         NOT NULL,
-cost                INTEGER         NOT NULL,
-location            VARCHAR(255)    NOT NULL,
-date                DATE            NOT NULL,
-orderStatus         VARCHAR(50)     NOT NULL,
+orderID             INTEGER                         NOT NULL,
+deliveryFee         INTEGER                         NOT NULL,
+cost                INTEGER                         NOT NULL,
+location            VARCHAR(255)                    NOT NULL,
+date                DATE                            NOT NULL,
+orderStatus         VARCHAR(50) DEFAULT 'Pending'   NOT NULL CHECK (orderStatus in ('Pending','Confirmed','Completed')),
 deliveryDuration    INTEGER         NOT NULL,
 timeOrderPlace      TIME,
 timeDepartToRest    TIME,
@@ -55,15 +54,16 @@ quantity            INTEGER
 promotionID         INTEGER,
 
 PRIMARY KEY (orderID),
-FOREIGN KEY (restaurantID,foodName,promotionID,quantity) REFERENCES FROM (restaurantID,foodName,promotionID,quantity)
+FOREIGN KEY (restaurantID,foodName,promotionID,quantity) REFERENCES FromMenu (restaurantID,foodName,promotionID,quantity) ON DELETE CASCADE
 );
 
-CREATE TABLE From (
+CREATE TABLE FromMenu (
 promotionID     INTEGER,
 quantity        INTEGER         NOT NULL,
 orderID         INTEGER         NOT NULL,
 restaurantID    INTEGER         NOT NULL,
 foodName        VARCHAR(100)    NOT NULL,
 PRIMARY KEY (restaurantID,foodName),
-FOREIGN KEY (promotionID) REFERENCES ____
+FOREIGN KEY (promotionID) REFERENCES Restpromo (promoID),
+FOREIGN KEY (restaurantID, foodName) REFERENCES Menu (restaurantID, foodName) ON DELETE CASCADE
 );
